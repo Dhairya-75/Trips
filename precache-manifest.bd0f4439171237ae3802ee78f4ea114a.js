faa18561ea4087cf4d57c86530537c79448954d7
Dhairya Deshmukh <dhairyadeshmukh75@gmail.com>
2020-06-20 11:38:45 +0530

    Updates

---
+++ precache-manifest.bd0f4439171237ae3802ee78f4ea114a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dcd19d9bf83874566dc5ebcefa7072e",
+    "url": "/Trips/index.html"
+  },
+  {
+    "revision": "868f1da2bfaca3eda9e2",
+    "url": "/Trips/static/css/main.a27509f0.chunk.css"
+  },
+  {
+    "revision": "5f61f243e16cc7a0ad99",
+    "url": "/Trips/static/js/2.eaa1d043.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Trips/static/js/2.eaa1d043.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "868f1da2bfaca3eda9e2",
+    "url": "/Trips/static/js/main.689394b7.chunk.js"
+  },
+  {
+    "revision": "a0b958f6774ae891b2f0",
+    "url": "/Trips/static/js/runtime-main.dad0fd3c.js"
+  },
+  {
+    "revision": "f150e1b87f19f66aba7df671f56e6dba",
+    "url": "/Trips/static/media/gradient.f150e1b8.jpg"
+  }
+]);
